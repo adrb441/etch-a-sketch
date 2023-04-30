f85cb6b8502fae27db2583d4fea8882d3e037089
adrb441 <38795409+adrb441@users.noreply.github.com>
2023-04-30 15:42:53 -0700

    Add function to change grid size

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-function createGrid(gridSize) {
+function createGrid(size) {
     const grid = document.querySelector('.container');
-    for (let i = 1; i <= gridSize * gridSize; i++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        grid.appendChild(square);
+    const gridSize = size * size;
+    for (let x = 0; x < size; x++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let y = 0; y < size; y++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            row.appendChild(square);
+        }
+        grid.appendChild(row);
     }
+    /*
+    const square = document.createElement('div');
+    square.classList.add('square');
+    grid.appendChild(square);*/
 }
 
 function changeSquareColor(e) {
@@ -14,5 +24,22 @@ function changeSquareColor(e) {
     }
 }
 
-createGrid(16);
+function changeGridSize(e) {
+    const gridSize = prompt("Enter new size for NxN grid:");
+    const grid = document.querySelector('.container');
+    if (gridSize === null) {
+        return;
+    } else if (isNaN(gridSize) || gridSize < 1 || gridSize > 100) {
+        alert("Out of range please enter a number between 1 and 100.");
+        return;
+    }
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+    createGrid(gridSize);
+}
+
+createGrid(3);
+const button = document.querySelector('#btn');
+button.addEventListener('click', changeGridSize);
 window.addEventListener('mouseover', changeSquareColor);
